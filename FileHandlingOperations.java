d459784763698b3b46d267861d26662bac0f6f3a
Yogesh Kanare <yogeshkanare85@gmail.com>
2023-03-13 18:15:19 +0530

    new file created

---
+++ FileHandlingOperations.java
@@ -0,0 +1,26 @@
+//create a file 
+import java.io.File;
+class FileHandlingOperations
+{
+	public static void main(String ar[])
+	{
+		File f1=null;
+		try
+		{
+	    f1=new File("file1.txt");
+
+	if(f1.createNewFile())
+	{
+		System.out.println("New File created");
+	}
+	else
+	{
+		System.out.println("File already exist");
+	}
+		}
+	catch(Exception e1)
+	{
+		System.out.println(e1);
+	}
+	}
+}
