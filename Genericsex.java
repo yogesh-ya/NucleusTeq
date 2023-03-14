afa072667ae5f9e9f438bdf48e02112f4f53964c
Yogesh Kanare <yogeshkanare85@gmail.com>
2023-03-14 12:01:27 +0530

    Solving problem of ClassCasteException using generics

--- Genericsex.java
+++ Genericsex.java
@@ -1,14 +1,14 @@
-//Problem of Class Caste Exception
+//Solving the problem of ClassCastException
 import java.util.*;
 class Genericsex
 {
 	public static void main(String ar[])
 	{	
-	ArrayList al=new ArrayList();
+	ArrayList<Integer> al=new ArrayList<>();
 	al.add(120);
 	al.add(20);
 	al.add(30);
-	al.add("40");
+	al.add("40");//compile time error
 	al.add(null);
 	Integer i=(Integer)al.get(3);
 	System.out.println(i);
