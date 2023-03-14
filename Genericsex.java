00f3b93b3cd14b090c47b6d04ef394ededb92e6f
Yogesh Kanare <yogeshkanare85@gmail.com>
2023-03-14 11:12:33 +0530

    storing any type of data not when genrics not used

---
+++ Genericsex.java
@@ -0,0 +1,15 @@
+//Program before generics
+import java.util.*;
+class Genericsex
+{
+	public static void main(String ar[])
+	{	
+	ArrayList al=new ArrayList();
+	al.add(10);
+	al.add(20);
+	al.add(30);
+	al.add("40");
+	al.add(50);
+	System.out.println(al);
+	}
+}
