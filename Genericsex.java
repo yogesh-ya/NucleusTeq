9159d0cd3adc708cc2c6fc3c17fc637285b79839
Yogesh Kanare <yogeshkanare85@gmail.com>
2023-03-14 11:59:21 +0530

    Problem of ClassCasteException when generics not used

--- Genericsex.java
+++ Genericsex.java
@@ -1,16 +1,16 @@
-//Solving the problem of type casting using genrics
+//Problem of Class Caste Exception
 import java.util.*;
 class Genericsex
 {
 	public static void main(String ar[])
 	{	
-	ArrayList<Integer> al=new ArrayList<>();
+	ArrayList al=new ArrayList();
 	al.add(120);
 	al.add(20);
 	al.add(30);
-	al.add(40);
+	al.add("40");
 	al.add(null);
-	Integer i=al.get(0);
+	Integer i=(Integer)al.get(3);
 	System.out.println(i);
 	}
 }
