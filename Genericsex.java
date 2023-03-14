8375abdedfdae90f8e6d359f04c824f454bd9230
Yogesh Kanare <yogeshkanare85@gmail.com>
2023-03-14 10:57:58 +0530

    storing any type of object before generics

---
+++ Genericsex.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileWriter;
+//Program before genrics
+import java.util.*;
+class Genericsex
+{
+	public static void main(String ar[])
+	{	
+	ArrayList al=new ArrayList();
+	al.add(10);
+	al.add(20);
+	al.add(30);
+	al.add("40");
+	al.add(50);
+	System.out.println(al);
+	}
+}
