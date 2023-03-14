50908c132742438b468092f5e848652cc8d416e7
Yogesh Kanare <yogeshkanare85@gmail.com>
2023-03-14 11:45:23 +0530

    storing specif type of data using generics

--- Genericsex.java
+++ Genericsex.java
@@ -1,14 +1,14 @@
-//Program before generics
+//Program after generics
 import java.util.*;
 class Genericsex
 {
 	public static void main(String ar[])
 	{	
-	ArrayList al=new ArrayList();
+	ArrayList<Integer> al=new ArrayList<>();
 	al.add(10);
 	al.add(20);
-	al.add(30);//integer type object
-	al.add("40");//String type object
+	al.add(30);
+	al.add(40);
 	al.add(null);
 	System.out.println(al);
 	}
