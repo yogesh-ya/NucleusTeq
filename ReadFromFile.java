64e488063f80f004b82fce75ce0b5ac57b5bfabb
Yogesh Kanare <yogeshkanare85@gmail.com>
2023-03-16 19:04:56 +0530

    creating file and getting data

--- ReadFromFile.java
+++ ReadFromFile.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -5,13 +6,17 @@ class ReadFromFile
 {
 	public static void main(String ar[])
 	{	
+	FileInputStream input=null;
 	try
 	{
+	File f=new File("Writablefile.txt");
+	f.createNewFile();
 	input=new FileInputStream("Writablefile.txt");
 	Scanner sc=new Scanner(input);
+	if(f.length()!=0)
 	System.out.println(sc.nextLine());
 	}
-	catch(FileNotFoundException e1)
+	catch(Exception e1)
 	{
 		System.out.println(e1);
 	}
